e7986cc69a706a92ba47608f3a4216b7cf0dd18e
rydershelley <rydershelley@hotmail.com>
2024-01-16 02:52:08 +0000

    initial push to repo

--- index.js
+++ index.js
@@ -24,7 +24,47 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//////////////////////
 
+//output format
+let resultTemplate = (validDate) => {
+  return {
+    unix: validDate.getTime(),
+    utc: validDate.toUTCString()
+  }
+}
+
+//process get requests
+app.get("/api/:date?", (req, res) => {
+  //catchall current time for empty input param
+  let date = new Date();
+  date = date.getTime(); //convert to milliseconds
+  
+
+  
+  if (!isNaN(req.params.date)) {
+    //if input is string of numbers convert to date as number
+    date = new Date(Number(req.params.date))
+  } else {
+    //otherwise return date as regular date format
+    date = new Date(req.params.date)
+  };
+
+  //if no input date leave output as current time
+  if (!req.params.date) {
+    date = new Date()
+  };
+  
+  //if input date is invalid format catch it and return 'invalid'
+  if (isNaN(date.getTime())) {
+    res.json({ error: "Invalid Date" })
+  };
+
+  res.send(resultTemplate(date));
+  });
+
+
+////////////////////////
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
